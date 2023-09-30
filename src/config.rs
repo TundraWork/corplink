4a70916c70b95e9faac8e1561b924be4dd337726
Arnie97 <arnie97@gmail.com>
2023-09-30 10:05:09 +0800

    Print verbose logs

--- src/config.rs
+++ src/config.rs
@@ -45,6 +45,7 @@ pub struct Config {
     pub state: Option<State>,
     pub vpn_server_name: Option<String>,
     pub vpn_select_strategy: Option<String>
+    pub vpn_route_full: Option<bool>,
 }
 
 impl fmt::Display for Config {
